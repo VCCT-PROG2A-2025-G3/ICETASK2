c88af3e57a867ba2223774d9406dee07c20f1bbb
AT10287107 <163420550+AT10287107@users.noreply.github.com>
2025-03-11 22:03:50 +0200

    Testing CI

---
+++ Music.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask2
+{
+    public class Music
+    {
+        private SoundPlayer _player;
+
+        public Music(string filePath)
+        {
+            // Remove any surrounding quotes from the file path
+            filePath = filePath.Trim();
+
+            // Validate the file path
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentException("File path cannot be null or whitespace.", nameof(filePath));
+            }
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("The specified file was not found.", filePath);
+            }
+
+            _player = new SoundPlayer(filePath);
+        }
+
+        public void Play()
+        {
+            _player.Play();
+        }
+
+        public void Stop()
+        {
+            _player.Stop();
+        }
+    }
+}
